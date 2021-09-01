9b455d330e909476d758ceb4a1039161dcfc62ab
Coulson5495 <82524030+Coulson5495@users.noreply.github.com>
2021-09-01 18:40:18 -0400

    SSD训练winderface

--- nets/ssd.py
+++ nets/ssd.py
@@ -1,95 +1,11 @@
 import torch
 import torch.nn as nn
-from nets.vgg import vgg as add_vgg
 import torch.nn.functional as F
-# from utils.config import Config
-from ssd_layer import L2Norm
+from utils.config import Config
 
-
-def add_extras(i, backbone_namne):
-    '''
-    输入extra layers 的通道数为1024
-    :param i: 输入此模块的通道
-    :param backbone_namne: backbone采用什么网络 vgg 或者轻量级网络
-    :return: 返回extra部分
-    '''
-
-    in_channels = i
-    layers = []
-    if backbone_namne == 'vgg':
-        # block 6
-        # 19,19,1024->10,10,512
-        layers += [nn.Conv2d(in_channels, 256, kernel_size=1, stride=1)]
-        layers += [nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1)]
-
-        # Block 7
-        # 10,10,512 -> 5,5,256
-        layers += [nn.Conv2d(512, 128, kernel_size=1, stride=1)]
-        layers += [nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)]
-
-        # Block 8
-        # 5,5,256 -> 3,3,256
-        layers += [nn.Conv2d(256, 128, kernel_size=1, stride=1)]
-        layers += [nn.Conv2d(128, 256, kernel_size=3, stride=1)]
-
-        # Block 9
-        # 3,3,256 -> 1,1,256
-        layers += [nn.Conv2d(256, 128, kernel_size=1, stride=1)]
-        layers += [nn.Conv2d(128, 256, kernel_size=3, stride=1)]
-    else:
-        pass
-
-    return layers
-
-
-def get_ssd(phase, num_classes, backbone_name, confidence=0.5, nms_iou=0.45):
-    '''
-
-    :param phase: training or test
-    :param num_class: 预测种类个数
-    :param backbone_name: 主干网络
-    :param confidence: 置信度
-    :param nms_iou: 非极大抑制的iou
-    :return:
-    '''
-
-    if backbone_name == 'vgg':
-        backbone, extra_layers = add_vgg(3), add_extras(1024, backbone_name)
-        mbox = [4, 6, 6, 6, 4, 4]
-    else:
-        pass
-
-    loc_layers = []  # 先验框的回归list
-    conf_layers = []  # 种类预测的list
-    if backbone_name == 'vgg':
-        # 首先对vgg中conv4中第三个卷积激活和conv5中第二个卷积激活进行先验框的回归和种类的预测
-        backbone_source = [21, -2]  # 21->conv4中第三个卷积激活; -2->conv5中第二个卷积激活
-        # ---------------------------------------------------#
-        #   在add_vgg获得的特征层里
-        #   第21层和-2层可以用来进行回归预测和分类预测。
-        #   分别是conv4-3(38,38,512)和conv7(19,19,1024)的输出
-        # ---------------------------------------------------#
-        for k, v in enumerate(backbone_source):
-            # 位置回归
-            loc_layers += [nn.Conv2d(backbone[v].out_channels,
-                                     mbox[k] * 4, kernel_size=3, padding=1)]
-            # 上面mbox[k]*4的含义是：一共有mbox[k]个框，每个先验框需要四个维度，即x,y,w,h
-
-            conf_layers += [nn.Conv2d(backbone[v].out_channels,
-                                      mbox[k] * num_classes, kernel_size=3, padding=1)]
-
-        # -------------------------------------------------------------#
-        #   在add_extras获得的特征层里
-        #   第1层、第3层、第5层、第7层可以用来进行回归预测和分类预测。
-        #   shape分别为(10,10,512), (5,5,256), (3,3,256), (1,1,256)
-        # -------------------------------------------------------------#
-        for k, v in enumerate(extra_layers[1::2], 2):
-            loc_layers += [nn.Conv2d(v.out_channels, mbox[k]
-                                     * 4, kernel_size=3, padding=1)]
-            conf_layers += [nn.Conv2d(v.out_channels, mbox[k]
-                                      * num_classes, kernel_size=3, padding=1)]
-    else:
-        pass
+from nets.mobilenetv2 import InvertedResidual, mobilenet_v2
+from nets.ssd_layers import Detect, L2Norm, PriorBox
+from nets.vgg import vgg as add_vgg
 
 
 class SSD(nn.Module):
@@ -97,21 +13,26 @@ class SSD(nn.Module):
         super(SSD, self).__init__()
         self.phase = phase
         self.num_classes = num_classes
-        self.confidence = confidence
-        self.nms_iou = nms_iou
-        self.backbone_name = backbone_name
-
-        # self.cfg = Config
-        if backbone_name == 'vgg':
+        self.cfg = Config
+        if backbone_name == "vgg":
             self.vgg = nn.ModuleList(base)
             self.L2Norm = L2Norm(512, 20)
         else:
-            pass
-        self.extras = nn.ModuleList(extras)
-        # 未完成
+            self.mobilenet = base
+            self.L2Norm = L2Norm(96, 20)
 
-    def forward(self):
-        # 将backbone与extras连接起来
+        self.extras = nn.ModuleList(extras)
+        self.priorbox = PriorBox(backbone_name, self.cfg)
+        with torch.no_grad():
+            self.priors = torch.tensor(self.priorbox.forward()).type(torch.FloatTensor)
+        self.loc = nn.ModuleList(head[0])
+        self.conf = nn.ModuleList(head[1])
+        self.backbone_name = backbone_name
+        if phase == 'test':
+            self.softmax = nn.Softmax(dim=-1)
+            self.detect = Detect(num_classes, 0, 200, confidence, nms_iou)
+
+    def forward(self, x):
         sources = list()
         loc = list()
         conf = list()
@@ -120,28 +41,31 @@ class SSD(nn.Module):
         #   获得conv4_3的内容
         #   shape为38,38,512
         # ---------------------------#
-        if self.backbone_name == 'vgg':
+        if self.backbone_name == "vgg":
             for k in range(23):
                 x = self.vgg[k](x)
         else:
-            pass
+            for k in range(14):
+                x = self.mobilenet[k](x)
         # ---------------------------#
         #   conv4_3的内容
         #   需要进行L2标准化
         # ---------------------------#
-        sources.append(self.L2Norm(x))
+        s = self.L2Norm(x)
+        sources.append(s)
 
         # ---------------------------#
         #   获得conv7的内容
         #   shape为19,19,1024
         # ---------------------------#
-        if self.backbone_name == 'vgg':
+        if self.backbone_name == "vgg":
             for k in range(23, len(self.vgg)):
                 x = self.vgg[k](x)
-            else:
-                pass
-        sources.append(x)
+        else:
+            for k in range(14, len(self.mobilenet)):
+                x = self.mobilenet[k](x)
 
+        sources.append(x)
         # -------------------------------------------------------------#
         #   在add_extras获得的特征层里
         #   第1层、第3层、第5层、第7层可以用来进行回归预测和分类预测。
@@ -153,7 +77,7 @@ class SSD(nn.Module):
                 if k % 2 == 1:
                     sources.append(x)
             else:
-                pass
+                sources.append(x)
 
         # -------------------------------------------------------------#
         #   为获得的6个有效特征层添加回归预测和分类预测
@@ -186,3 +110,102 @@ class SSD(nn.Module):
                 self.priors
             )
         return output
+
+
+def add_extras(i, backbone_name):
+    layers = []
+    in_channels = i
+
+    if backbone_name == 'vgg':
+        # Block 6
+        # 19,19,1024 -> 10,10,512
+        layers += [nn.Conv2d(in_channels, 256, kernel_size=1, stride=1)]
+        layers += [nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1)]
+
+        # Block 7
+        # 10,10,512 -> 5,5,256
+        layers += [nn.Conv2d(512, 128, kernel_size=1, stride=1)]
+        layers += [nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)]
+
+        # Block 8
+        # 5,5,256 -> 3,3,256
+        layers += [nn.Conv2d(256, 128, kernel_size=1, stride=1)]
+        layers += [nn.Conv2d(128, 256, kernel_size=3, stride=1)]
+
+        # Block 9
+        # 3,3,256 -> 1,1,256
+        layers += [nn.Conv2d(256, 128, kernel_size=1, stride=1)]
+        layers += [nn.Conv2d(128, 256, kernel_size=3, stride=1)]
+    else:
+        layers += [InvertedResidual(in_channels, 512, stride=2, expand_ratio=0.2)]
+        layers += [InvertedResidual(512, 256, stride=2, expand_ratio=0.25)]
+        layers += [InvertedResidual(256, 256, stride=2, expand_ratio=0.5)]
+        layers += [InvertedResidual(256, 64, stride=2, expand_ratio=0.25)]
+
+    return layers
+
+
+def get_ssd(phase, num_classes, backbone_name, confidence=0.5, nms_iou=0.45):
+    # ---------------------------------------------------#
+    #   add_vgg指的是加入vgg主干特征提取网络。
+    #   该网络的最后一个特征层是conv7后的结果。
+    #   shape为19,19,1024。
+    #
+    #   为了更好的提取出特征用于预测。
+    #   SSD网络会继续进行下采样。
+    #   add_extras是额外下采样的部分。   
+    # ---------------------------------------------------#
+    if backbone_name == 'vgg':
+        backbone, extra_layers = add_vgg(3), add_extras(1024, backbone_name)
+        mbox = [4, 6, 6, 6, 4, 4]
+    else:
+        backbone, extra_layers = mobilenet_v2().features, add_extras(1280, backbone_name)
+        mbox = [6, 6, 6, 6, 6, 6]
+
+    loc_layers = []
+    conf_layers = []
+
+    if backbone_name == 'vgg':
+        backbone_source = [21, -2]
+        # ---------------------------------------------------#
+        #   在add_vgg获得的特征层里
+        #   第21层和-2层可以用来进行回归预测和分类预测。
+        #   分别是conv4-3(38,38,512)和conv7(19,19,1024)的输出
+        # ---------------------------------------------------#
+        for k, v in enumerate(backbone_source):
+            loc_layers += [nn.Conv2d(backbone[v].out_channels,
+                                     mbox[k] * 4, kernel_size=3, padding=1)]
+            conf_layers += [nn.Conv2d(backbone[v].out_channels,
+                                      mbox[k] * num_classes, kernel_size=3, padding=1)]
+        # -------------------------------------------------------------#
+        #   在add_extras获得的特征层里
+        #   第1层、第3层、第5层、第7层可以用来进行回归预测和分类预测。
+        #   shape分别为(10,10,512), (5,5,256), (3,3,256), (1,1,256)
+        # -------------------------------------------------------------#
+        for k, v in enumerate(extra_layers[1::2], 2):
+            loc_layers += [nn.Conv2d(v.out_channels, mbox[k]
+                                     * 4, kernel_size=3, padding=1)]
+            conf_layers += [nn.Conv2d(v.out_channels, mbox[k]
+                                      * num_classes, kernel_size=3, padding=1)]
+    else:
+        backbone_source = [13, -1]
+        for k, v in enumerate(backbone_source):
+            loc_layers += [nn.Conv2d(backbone[v].out_channels,
+                                     mbox[k] * 4, kernel_size=3, padding=1)]
+            conf_layers += [nn.Conv2d(backbone[v].out_channels,
+                                      mbox[k] * num_classes, kernel_size=3, padding=1)]
+
+        for k, v in enumerate(extra_layers, 2):
+            loc_layers += [nn.Conv2d(v.out_channels, mbox[k]
+                                     * 4, kernel_size=3, padding=1)]
+            conf_layers += [nn.Conv2d(v.out_channels, mbox[k]
+                                      * num_classes, kernel_size=3, padding=1)]
+
+    # -------------------------------------------------------------#
+    #   add_vgg和add_extras，一共获得了6个有效特征层，shape分别为：
+    #   (38,38,512), (19,19,1024), (10,10,512), 
+    #   (5,5,256), (3,3,256), (1,1,256)
+    # -------------------------------------------------------------#
+    SSD_MODEL = SSD(phase, backbone, extra_layers, (loc_layers, conf_layers), num_classes, confidence, nms_iou,
+                    backbone_name)
+    return SSD_MODEL
--- nets/vgg.py
+++ nets/vgg.py
@@ -1,5 +1,7 @@
-import torch
 import torch.nn as nn
+
+base = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512, 'M',
+            512, 512, 512]
 
 '''
 该代码用于获得VGG主干特征提取网络的输出。
@@ -10,28 +12,8 @@ import torch.nn as nn
 -> 38,38,256 -> 38,38,512 -> 38,38,512 -> 38,38,512 -> 19,19,512 ->  19,19,512 ->  19,19,512 -> 19,19,512
 到base结束，我们获得了一个19,19,512的特征层
 
-a、输入一张图片后，被resize到300x300的shape
-
-b、conv1，经过两次[3,3]卷积网络，输出的特征层为64，输出为(300,300,64)，再2X2最大池化，该最大池化步长为2，输出net为(150,150,64)。
-
-c、conv2，经过两次[3,3]卷积网络，输出的特征层为128，输出net为(150,150,128)，再2X2最大池化，该最大池化步长为2，输出net为(75,75,128)。
-
-d、conv3，经过三次[3,3]卷积网络，输出的特征层为256，输出net为(75,75,256)，再2X2最大池化，该最大池化步长为2，输出net为(38,38,256)。
-
-e、conv4，经过三次[3,3]卷积网络，输出的特征层为512，输出net为(38,38,512)，再2X2最大池化，该最大池化步长为2，输出net为(19,19,512)。
-    conv4中第三个卷积激活后会进行先验框的回归和种类的预测
-
-f、conv5，经过三次[3,3]卷积网络，输出的特征层为512，输出net为(19,19,512)，再3X3最大池化，该最大池化步长为1，输出net为(19,19,512)。
-    conv5中第二个卷积激活后会新型先验框的回归和种类的预测
-
-
 之后进行pool5、conv6、conv7。
 '''
-
-base = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512, 'M',
-        512, 512, 512]  # M是最大池化层；C是inplace=true
-
-
 def vgg(i):
     layers = []
     in_channels = i
@@ -47,10 +29,6 @@ def vgg(i):
     pool5 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
     conv6 = nn.Conv2d(512, 1024, kernel_size=3, padding=6, dilation=6)
     conv7 = nn.Conv2d(1024, 1024, kernel_size=1)
-    layers += [pool5, conv6, nn.ReLU(inplace=True), conv7, nn.ReLU(inplace=7)]
+    layers += [pool5, conv6,
+               nn.ReLU(inplace=True), conv7, nn.ReLU(inplace=True)]
     return layers
-
-
-if __name__ == "__main__":
-    vgg_net = vgg(3)
-    print(len(vgg_net))
